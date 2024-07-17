5332a10b589a7a9b9153977f7e5cb7974a3d82d6
Nitu Choudhary <learningadventureswithnitu@gmail.com>
2024-07-17 12:42:07 -0400

    Add backend after backend resources were created

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,24 @@
 terraform {
-  required_version = ">= 1.2.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    required_version = ">= 1.2.0"
+  
+    backend "s3" {
+        bucket         = "dapp-terraform-state"
+        key            = "tf-infra/terraform.tfstate"
+        region         = "us-east-1"
+        dynamodb_table = "dappTFState"
+        encrypt        = true
     }
-  }
+
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.16"
+        }
+    }
 }
 
 module "tfState" {
-  source      = "./modules/tf-state"
-  bucket_name = "dapp-terraform-state"
-  table_name  = "dappTFState"
+    source      = "./modules/tf-state"
+    bucket_name = "dapp-terraform-state"
+    table_name  = "dappTFState"
 }
